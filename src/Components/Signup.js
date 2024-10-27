67350d9fa816616c11c1cf9a02f34e99fdfcddc7
rahul1574 <166401965+rahul1574@users.noreply.github.com>
2024-10-27 16:54:51 +0530

    Update Signup.js

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -38,7 +38,7 @@ function Signup() {
         if (!validateInputs()) return;
 
         try {
-            const response = await axios.post('http://phpweb.infinityfreeapp.com/index.php', inputs);
+            const response = await axios.post('https://phpweb.infinityfreeapp.com/index.php'', inputs);
             setMessage(response.data.message);
             // console.log(response.data.status);
             if (response.data.status === 1) {
@@ -52,7 +52,7 @@ function Signup() {
     const handleLogin = async (event) => {
         event.preventDefault();
         try {
-            const response = await axios.post('http://phpweb.infinityfreeapp.com/index.php', inputs);
+            const response = await axios.post('https://phpweb.infinityfreeapp.com/index.php', inputs);
             setMessage(response.data.message);
             if (response.data.status === 1) {
                 const userDetails = response.data.user;
